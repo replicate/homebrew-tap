5f9354d1afd08f1c99beb73a0c71c181072a32e6
Mattt <mattt@replicate.com>
2023-08-25 05:10:46 -0700

    Add pget formula (#6)
    * Add pget formula
    
    * Add checksum
    
    * Update test
    
    * Test status of pget --help
    
    ---------
    
    Co-authored-by: Replicate Test Bot <noreply@replicate.com>

---
+++ Formula/pget.rb
@@ -0,0 +1,20 @@
+class Pget < Formula
+  desc "Parallel file downloader & extractor"
+  homepage "https://github.com/replicate/pget"
+  url "https://github.com/replicate/pget/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "a93081a037d979fa8af4900aaea0e56ca11d71cb2fe2c326873c57a6d527e040"
+
+  license "Apache-2.0"
+  head "https://github.com/replicate/pget.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "make"
+    bin.install "pget"
+  end
+
+  test do
+    shell_output("#{bin}/pget --help")
+  end
+end
