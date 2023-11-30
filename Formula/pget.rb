9c88954fea355d4834db46a59a6e4634f0bf3574
Morgan Fainberg <morgan@replicate.com>
2023-11-29 21:40:16 -0800

    pget 0.2.1

--- Formula/pget.rb
+++ Formula/pget.rb
@@ -1,8 +1,8 @@
 class Pget < Formula
   desc "Parallel file downloader & extractor"
   homepage "https://github.com/replicate/pget"
-  url "https://github.com/replicate/pget/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "fbd57a42fc7062c735d300c417b9df1a672bd2f8f102d81153ac1d65f1697a7c"
+  url "https://github.com/replicate/pget/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "332ceee96dd2d3fd66ee03f51b334ac8ff140ad0439ededfeba425becab0796c"
 
   license "Apache-2.0"
   head "https://github.com/replicate/pget.git", branch: "main"
